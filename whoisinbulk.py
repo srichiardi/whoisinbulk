f22fa9bb1f37e1dbda98f0c95f486a827a52faa2
srichiardi <s.richiardi@gmail.com>
2015-09-03 17:14:52 +0100

    first lines of code

--- whoisinbulk.py
+++ whoisinbulk.py
@@ -1,8 +1,33 @@
 import whois
+import re
 
 
 class Whoisit:
     
-    def __init__(self, listOfUrls):
-      pass
-      
+    
+    def __init__(self):
+        self.domainsDict = {}
+        self.headers = []
+    
+    
+    def retrieve(self, domainsList):
+        
+        for domain in domainsList:
+            try:
+                rawText = whois.whois(domain)
+                
+            except Exception:
+                self.domainsDict[domain] = {'ErrorCode' : 'Unable to retrieve data'}
+                
+            else:
+                reList = []
+                
+                for line in rawText.split('\n'):
+                    matchStr = re.findall(r'(\w+\s\w+\s?\w*\s?\w*\s?:[^\\\\]\s*.*)',line.replace(r'\r',''))
+                    reList.extend(matchStr)
+                
+            
+    
+    
+    def exportToCsv(self):
+        pass
